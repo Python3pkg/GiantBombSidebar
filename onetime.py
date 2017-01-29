25bbcc3865d85eee6baf50def94a5410ba6ede46
Steven Fortier <stevendfortier@yahoo.com>
2017-01-29 00:37:35 -0500

    Updated to PRAW4

--- onetime.py
+++ onetime.py
@@ -1,6 +1,3 @@
-import praw
-from prawoauth2 import PrawOAuth2Server
-
 print '\nIf not set up, please create your application at:'
 print 'https://www.reddit.com/prefs/apps/'
 print 'Make sure you set it as a script and have the redirect uri be:'
@@ -10,21 +7,16 @@ app_key_input = raw_input('Please enter your app key: ')
 app_secret_input = raw_input('Please enter your app secret: ')
 app_name_input = raw_input('Please enter the name you used for the app: ')
 subreddit_input = raw_input('Please enter your subreddit: ')
-username_input = raw_input('Please enter your username (for the user-agent): ')
+username_input = raw_input('Please enter your username: ')
 user_agent = 'python:' + app_name_input + ':* (by /u/' + username_input + ')'
-print 'Your web browser will now open, please accept the agreement'
-
-r = praw.Reddit(user_agent = user_agent)
-o = PrawOAuth2Server(r, app_key = app_key_input, app_secret = app_secret_input, state = user_agent, scopes = ['identity', 'modconfig'])
-o.start()
-tokens = o.get_access_codes()
+password_input = raw_input('Please enter your password: ')
 
 settings = open('settings.py', 'w+')
 settings.write('app_key = \'' + app_key_input + '\'')
 settings.write('\napp_secret = \'' + app_secret_input + '\'')
-settings.write('\naccess_token = \'' + tokens['access_token'] + '\'')
-settings.write('\nrefresh_token = \''  + tokens['refresh_token'] + '\'')
-settings.write('\nsubreddit = \'' + subreddit_input + '\'')
+settings.write('\nsubreddit_name = \'' + subreddit_input + '\'')
+settings.write('\nusername = \'' + username_input + '\'')
+settings.write('\npassword = \'' + password_input + '\'')
 settings.write('\nuser_agent = \'' + user_agent + '\'')
 settings.close()
 
